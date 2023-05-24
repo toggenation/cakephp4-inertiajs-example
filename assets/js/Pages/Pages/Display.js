9b45f750dcea86c70fa62caf93dff661318d696d
James McDonald <james@toggen.com.au>
2023-05-25 08:41:06 +1000

    Fix missing template

--- assets/js/Pages/Pages/Display.js
+++ assets/js/Pages/Pages/Display.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import Layout from '../../layouts/Layout';
 
 /**
  * This view component will be rendered when visiting /pages/greet
  * when your application has been setup with inertiajs.
  */
-export default function Display() {
-    return <div>Hello world!</div>;
+function Display() {
+
+    return <>
+        <h2>Hello world!</h2>
+    </>
 }
+
+
+Display.layout = page => <Layout children={page} title="Hi" />
+
+export default Display;
+
+
--- assets/js/layouts/Layout.js
+++ assets/js/layouts/Layout.js
@@ -2,33 +2,25 @@ import { Link } from '@inertiajs/react'
 
 export default function Layout({ children }) {
     return (
-        <div className='row'>
-            <div className='col-2'>
-                <ul className="nav flex-column">
-                    <li className="nav-item">
-                        <Link className='nav-link' href="/">Home</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className='nav-link' href="/about">About</Link>
-                    </li>
-                    <li className="nav-item">
+        <div className='container'>
+            <div className='row mt-4'>
+                <div className='col-2'>
+                    <ul className="nav flex-column">
+                        <li className="nav-item">
+                            <Link className='nav-link' href="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className='nav-link' href="/items">Items Index</Link>
+                        </li>
 
-                        <Link className='nav-link' href="/contact">Contact</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className='nav-link' href="/items/red">Redirect</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className='nav-link' href="/items">Items Index</Link>
-                    </li>
-
-                    <li className="nav-item">
-                        <Link className='nav-link' href="/items/different-action-same-component">Same Component</Link>
-                    </li>
-                </ul>
-            </div>
-            <div className='col'>
-                {children}
+                        <li className="nav-item">
+                            <Link className='nav-link' href="/items/different-action-same-component">Same Component</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='col'>
+                    {children}
+                </div>
             </div>
         </div>
     )
